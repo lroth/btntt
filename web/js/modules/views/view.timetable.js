efb6fe46bc3ef70531821feb2dc70d3a4b9b7d07
lroth <lukasz.roth@gmail.com>
2012-10-08 00:06:39 +0200

    datepicker in progress

--- web/js/modules/views/view.timetable.js
+++ web/js/modules/views/view.timetable.js
@@ -2,12 +2,18 @@ define('views/view.timetable', [], function () {
     var timeTable = {};
 
     activateDateSelector = function() {
+
+        //regular buttons
         $('button.select_date').click(function(e) {
             $('button.select_date').removeClass('active');
             e.preventDefault();
             $(this).addClass('active');
             $('.timeContainer').val($(this).attr('data-value'));
         })
+
+        //calendar selector
+
+
     }
 
     timeTable.init = function(el) {
